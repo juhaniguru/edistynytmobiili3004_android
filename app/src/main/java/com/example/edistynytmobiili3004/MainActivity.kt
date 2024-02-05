7e6d28d3704e52b7dae14d68a78c500fd2cc1a89
juhani.kuru <juhani.kuru@lapinamk.fi>
2024-02-05 19:40:14 +0200

    add loginvm, add navigation drawer

--- app/src/main/java/com/example/edistynytmobiili3004/MainActivity.kt
+++ app/src/main/java/com/example/edistynytmobiili3004/MainActivity.kt
@@ -4,26 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
+
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 
 
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.edistynytmobiili3004.ui.theme.EdistynytMobiili3004Theme
+import com.example.edistynytmobiili3004.viewmodel.LoginViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,35 +54,32 @@ class MainActivity : ComponentActivity() {
 
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
+                    val scope = rememberCoroutineScope()
+                    ModalNavigationDrawer(
+                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding),
+                        drawerContent = {
+                            ModalDrawerSheet {
+                                Spacer(modifier = Modifier.height(16.dp))
+                                NavigationDrawerItem(
+                                    label = { Text(text = "Categories") },
+                                    selected = true,
+                                    onClick = { /*TODO*/ }, icon = {
+                                        Icon(
+                                            imageVector = Icons.Filled.Home,
+                                            contentDescription = "Home"
+                                        )
+                                    })
+                            }
+                        }, drawerState = drawerState) {
+                        Text("Welcome Home")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun LoginScreen() {
-    val password = remember {
-        mutableStateOf("")
-    }
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        OutlinedTextField(value = "", onValueChange = { newUsername ->
 
-        }, placeholder = {
-            Text(text = "Username")
-        })
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = password.value, onValueChange = { newPassword ->
-            password.value = newPassword
-        }, placeholder = {
-            Text(text = "Password")
-        }, visualTransformation = PasswordVisualTransformation())
-
-    }
-}
 
 
